cb5abee622499d57af0c3325dcfacd4ecd937054
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-04 22:05:21 +0300

    Merge branch 'main'

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Image
+from .models import Image,Brand,Category,Product
 
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
@@ -10,3 +16,12 @@ class ImageAdmin(admin.ModelAdmin):
 
     def name(self, obj):
         return obj.product.name
+
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra=1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+    list_display = ['name','price', 'stocked']
