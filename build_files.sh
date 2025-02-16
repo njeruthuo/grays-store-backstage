b687ea9d1e2969e878bfc3ef7a685a73bfdc7fd6
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-16 07:43:02 +0300

    pip installation

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,16 @@
 # build_files.sh
 
+# Install pip if not available
+if ! command -v pip &> /dev/null; then
+    echo "Installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+fi
+
+echo "INSTALLING REQUIREMENTS....."
+
 pip install -r requirements.txt
+
+echo "Done..."
+
 python3.12 manage.py collectstatic --noinput
