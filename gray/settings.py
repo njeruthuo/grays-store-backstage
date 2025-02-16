c51ca59a25442963cf991632123c74374cddcc98
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-16 22:22:08 +0300

    cors origin for frontend requests

--- gray/settings.py
+++ gray/settings.py
@@ -166,7 +166,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173', "https://sandbox.safaricom.co.ke",
-    "https://api.safaricom.co.ke", 'https://9b9b-102-219-209-62.ngrok-free.app'
+    "https://api.safaricom.co.ke", 'https://9b9b-102-219-209-62.ngrok-free.app', "https://grays-online-store.vercel.app/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # ✅ Needed if using authentication/cookies
