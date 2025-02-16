19aeebd94059ed357a755ccc2d6eee1ff8a4da5d
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-16 07:44:40 +0300

    build file change

--- build_files.sh
+++ build_files.sh
@@ -9,8 +9,8 @@ fi
 
 echo "INSTALLING REQUIREMENTS....."
 
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Done..."
 
-python3.12 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
