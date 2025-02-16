12d4a02384b4d6f6951db6e50512f9af2178751c
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-16 07:39:30 +0300

    load-env file

--- gray/wsgi.py
+++ gray/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import dotenv
-
-dotenv.read_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gray.settings')
