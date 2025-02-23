51285ee2c7a90fcf2c49fb8cafb7c9a3deb80bc1
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-23 17:07:55 +0300

    wss attempt

--- payment/routing.py
+++ payment/routing.py
@@ -3,5 +3,5 @@ from .consumers import PaymentConsumer, MPESAConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/payments/$", PaymentConsumer.as_asgi()),
-    re_path(r"ws/mpesa/$", MPESAConsumer.as_asgi()),
+    re_path(r"wss/mpesa/$", MPESAConsumer.as_asgi()),
 ]
