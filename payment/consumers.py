fb4f32da6d784d99d5ec7f8ef095b6dfcdafa37d
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-23 17:23:28 +0300

    consumers and urls

--- payment/consumers.py
+++ payment/consumers.py
@@ -3,23 +3,6 @@ from .views import stk_push
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class PaymentConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.group_name = "payments"
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
-
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        await self.send(text_data=json.dumps({"message": "Received: " + data["message"]}))
-
-    async def send_payment_update(self, event):
-        await self.send(text_data=json.dumps(event["data"]))
-
-
 class MPESAConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.group_name = "mpesa_payments"
--- payment/routing.py
+++ payment/routing.py
@@ -1,7 +1,6 @@
 from django.urls import re_path
-from .consumers import PaymentConsumer, MPESAConsumer
+from .consumers import  MPESAConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/payments/$", PaymentConsumer.as_asgi()),
     re_path(r"wss/mpesa/$", MPESAConsumer.as_asgi()),
 ]
