defac26d7779af8ce0f586559dc98746ea899e3f
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-28 17:41:30 +0300

    modify bin/start

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-daphne --bind 127.0.0.1 -p 8000 gray.asgi:application
+daphne gray.asgi:application
 
+# daphne --bind 127.0.0.1 -p 8000 gray.asgi:application
 # gunicorn gray.wsgi --log-file -
